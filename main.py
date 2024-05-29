5f1144bcd51c18be5b42476b3ae9586d9552786b
Devendra <dekuta75@gmail.com>
2024-05-29 09:45:35 +0530

    added uvicorn.run in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI,Depends
 from fastapi.middleware.cors import CORSMiddleware
-
+from os import getenv
 from auth import get_current_user
 from routers import todo, user
 
@@ -21,3 +21,8 @@ app.include_router(user.router, prefix="/users", tags=["users"])
 @app.get("/")
 async def root():
     return {"message": "Welcome to the TODO app"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port=int(getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port,reload=True)
